00d24bbb4a9e606eb8f11ae19e7057571518598d
Jim Smith <jim@ponder-stibbons.com>
2023-06-23 11:38:31 +0100

    Improved logging

--- sys/bin/cpio_backup.sh
+++ sys/bin/cpio_backup.sh
@@ -21,6 +21,23 @@ case $BACKUP_TARGET in
 esac
 
 date >>${LOGFILE}
+ERR_MSG=""
+if [ ! -d ${BACKUP_ROOT} ];then
+        ERR_MSG="Backup volume not mounted"
+fi
+if [ ! -d ${BACKUP_ROOT}/${BACKUP_TYPE} ];then
+        ERR_MSG="Backup directory absent"
+fi
+if [ ! -z "${ERR_MSG}" ];then
+         echo "${ERR_MSG} - ${BACKUP_ROOT}/${BACKUP_TYPE}" >>${LOGFILE}
+        echo ${ERR_MSG} |
+        mail -s "${ERR_MSG} - ${BACKUP_ROOT}/${BACKUP_TYPE}" \
+                -r backups@aqulia-eth \
+                jim@ponder-stibbons.com
+        exit 1
+fi
+
+
 if [  -d ${BACKUP_DIR} ];then
 	echo  "cd "${BACKUP_TARGET}"; find . |cpio -pdmv ${BACKUP_DIR}" >>${LOGFILE} 
 	touch ${FLAGFILE}
@@ -31,4 +48,4 @@ else
 
 fi
 date >>${LOGFILE}
-
+exit 0

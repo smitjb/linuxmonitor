d9a679b2fea8e03a590a98519d7c130d77a3c0b1
Jim Smith <jim@ponder-stibbons.com>
2023-07-08 08:55:33 +0100

    First production version of services monitoring

--- sys/bin/monitor_services.sh
+++ sys/bin/monitor_services.sh
@@ -1,21 +1,46 @@
-for host in $(cat monitor_services.ini | awk -F: '{ print $1 }')
+#!/bin/bash
+
+TIMESTAMP=$(date +%Y%m%d_%H%M%S)
+
+
+LOGROOT=/home/root/logs
+LOGFILE=${LOGROOT}/monitor_services_${TIMESTAMP}.log
+
+this_dir=$(dirname ${0})
+
+echo "this_dir:[$this_dir]"
+cfg_dir="$this_dir/../etc"
+
+cfg_file=$cfg_dir/monitor_services.ini
+
+REPORT_NEEDED="N"
+
+for host in $(cat ${cfg_file} | awk -F: '{ print $1 }')
 do
-	echo -n "$host ->"
+	echo -n "$host ->" >>${LOGFILE}
 	ping -c 1 $host >/dev/null
 	if [ $? == 0 ];then
-		echo "  pingable"
+		echo "  pingable" >>${LOGFILE}
 	else
-		echo "  not pingable"
+		echo "  not pingable" >>${LOGFILE}
+		REPORT_NEEDED="Y"
 	fi
-	for port in $(grep $host monitor_services.ini | awk -F: '{ print $2 }')
+	for port in $(grep $host ${cfg_file}| awk -F: '{ print $2 }')
 	do
 		nc -z  -4 ${host} ${port} #>/dev/null #2&>1
 		if [ $? == 0 ];then
-			echo OK ${host} ${port} open
+			echo OK ${host} ${port} open >>${LOGFILE}
 		else
-			echo FAIL ${host} ${port} not open
+			echo FAIL ${host} ${port} not open >>${LOGFILE}
+			REPORT_NEEDED="Y"
 		fi
 	done
 done
+if [ ${REPORT_NEEDED} == "Y" ];then
+         mail -s "Warning services missing" -r monitor@aquila-eth jim@ponder-stibbons.com <${LOGFILE}
+
+        cat ${LOGFILE}
+fi
+
 
 

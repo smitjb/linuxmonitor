e338c4a892fccc7e9a62aa4b62c2caf3842973f7
Jim Smith <jim@ponder-stibbons.com>
2023-10-26 17:09:38 +0100

    Changed recipient to smithb0809+monitor

--- sys/bin/gmail_check.sh
+++ sys/bin/gmail_check.sh
@@ -1,6 +1,9 @@
 
+RECIPIENT=smitjb0809+monitor@gmail.com
+SENDER=monitor@ponder-stibbons.com
+
 TIMESTAMP=$( date "+%d/%m/%Y %H:%M")
-mail -s "Gmail access check $TIMESTAMP" -r monitor@ponder-stibbons.com smitjb0809@gmail.com  <<EOT
+mail -s "Gmail access check $TIMESTAMP" -r ${SENDER} ${RECIPIENT}  <<EOT
 
 This mail is intended to test if jim@ponder-stibbons.com can send emails to gmail
 EOT
